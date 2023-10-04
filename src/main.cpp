c86883a964674a054e2d54f55708e3cf9e60ce01
Pavelka, Roman (ADV D EU CZ PDS1 CIO 1) <roman.pavelka@siemens.com>
2023-10-04 15:03:30 +0200

    Working with templates

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,20 @@
 #include <boost/di.hpp>
 namespace di = boost::di;
 
+class Named;
+
+template <class T = Named>
+class Exclamator {
+public:
+    Exclamator(T a): x{a} {}
+
+    void f() {
+        std::cout << x.name() << std::endl;
+    }
+private:
+    T x;
+};
+
 class A {
 public:
     std::string name() { return "A"; }
@@ -16,21 +30,13 @@ public:
 
 class C {} ;
 
-template <typename T>
-class Exclamator {
-public:
-    Exclamator(T a): x{a} {}
-
-    void f() {
-        std::cout << x.name() << std::endl;
-    }
-private:
-    T x;
-};
-
 int main() {
-//    auto injector = di::make_injector();
-//    injector.create();
+/*    auto injector = di::make_injector(
+        di::bind<Named>().to<A>()
+    );
+
+    auto xa = injector.create<Exclamator>();
+    xa.f();*/
 
     A a;
     Exclamator xa {a};
